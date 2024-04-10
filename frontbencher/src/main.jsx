8e83c08ce97fb318f0234b19d15c6f10abeb8464
bishakhne0gi <bneogi102002@gmail.com>
2024-04-10 23:33:02 +0530

    feat: folder structuring

--- frontbencher/src/main.jsx
+++ frontbencher/src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
 
+
+const router = createBrowserRouter(
+  [
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <App />
+        }
+      ]
+    }
+  ]
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <RouterProvider router={router} />
+
 )
