68f58ea9d35a54f5c0cd5cef090a895e37907970
soumit-s <soumit.srim@gmail.com>
2024-04-13 00:09:31 +0530

    Added swagger api docs. Added /api as prefix to each route. Made minor bug fixes.

--- backbencher/src/index.ts
+++ backbencher/src/index.ts
@@ -3,7 +3,10 @@ import cors from "cors";
 import { db } from "./db/client";
 import { placement_details } from "./db/schema";
 import { StatusCodes } from "http-status-codes";
-import { count, eq } from "drizzle-orm";
+import { count, eq, getTableColumns } from "drizzle-orm";
+import * as swaggerUI from "swagger-ui-express";
+import { default as swaggerDocument } from "./swagger.json";
+
 const PORT = 8000;
 const app: Express = express();
 
@@ -14,13 +17,14 @@ declare module "express-serve-static-core" {
 }
 
 app.use(cors());
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument, { explorer: true }));
 app.use(express.json());
 app.use((req, _, next) => {
   req.useDb = () => db;
   next();
 });
 
-app.get("/placement/count", async (req: Request, res: Response) => {
+app.get("/api/placement/count", async (req: Request, res: Response) => {
   const year = Number.parseInt(String(req.query.year ?? ""));
   if (Number.isNaN(year)) return res.sendStatus(StatusCodes.BAD_REQUEST);
   const [{ count: record_count }] = (await req
@@ -31,7 +35,7 @@ app.get("/placement/count", async (req: Request, res: Response) => {
   return res.json({ ok: true, data: { count: record_count } });
 });
 
-app.get("/placement/:year([0-9]+)", async (req: Request, res: Response) => {
+app.get("/api/placement/:year([0-9]+)", async (req: Request, res: Response) => {
   const year = Number.parseInt(req.params.year);
   const page = Number.parseInt(String(req.query.page ?? "1"));
   const page_size = Number.parseInt(String(req.query.size ?? "10"));
@@ -47,9 +51,10 @@ app.get("/placement/:year([0-9]+)", async (req: Request, res: Response) => {
     return res.sendStatus(StatusCodes.BAD_REQUEST);
   }
 
+  const { id: _, ...required_columns} = getTableColumns(placement_details);
   const records = await req
     .useDb?.()
-    .select()
+    .select(required_columns)
     .from(placement_details)
     .where(eq(placement_details.year, year))
     .orderBy(placement_details.slno)
@@ -58,8 +63,8 @@ app.get("/placement/:year([0-9]+)", async (req: Request, res: Response) => {
   return res.json({ ok: true, data: records });
 });
 
-app.get("/ping", (_, res: Response) => {
-  res.json({ ok: true, msg: "Hello World" });
+app.get("/api/ping", (_, res: Response) => {
+  res.json({ ok: true, data: "Hello World" });
 });
 
 app.listen(PORT, () => {
