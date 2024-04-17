58532af6075a87ad16e872e66a721a6fba389106
bishakhne0gi <bneogi102002@gmail.com>
2024-04-17 22:49:21 +0530

    feat: :fire: Favvy icon

--- frontbencher/src/app/page.js
+++ frontbencher/src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className='min-h-screen px-20 bg-white text-deep-blue flex flex-col justify-center items-center font-montserrat'>
       Its downhill from here, bow down before
-      <span className="font-geist ">
+      <span className="font-geistBlack ">
         soumitsayan
       </span>
     </main>
