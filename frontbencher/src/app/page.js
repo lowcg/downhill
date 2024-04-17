933c7c44fb331f80eefce211bf7f7e5520b760fc
Sayan67 <sayan.das.21@aot.edu.in>
2024-04-18 00:38:05 +0530

    Added Header Title component and empty Hero section and BG image

--- frontbencher/src/app/page.js
+++ frontbencher/src/app/page.js
@@ -1,10 +1,15 @@
+import HeaderTitle from "@/components/HeaderTitle";
+
 export default function Home() {
   return (
     <main className='min-h-screen px-20 bg-white text-deep-blue flex flex-col justify-center items-center font-montserrat'>
-      Its downhill from here, bow down before
-      <span className="font-geistBlack ">
-        soumitsayan
-      </span>
+
+        <div className="w-full flex justify-center h-full">
+          <HeaderTitle title={"Weekly Updates"} />
+        </div>
+      
+
+
     </main>
   );
 }
