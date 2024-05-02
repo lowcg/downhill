637f12ce39f3821335eea272e64840524bd303cd
Indrani Som <indranisom39@gmail.com>
2024-05-02 23:36:07 +0530

    bug fixing

--- frontbencher/src/components/deptnews.jsx
+++ frontbencher/src/components/deptnews.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import Subheading from "../components/Subheading";
-import NewsCard from './NewsCard';
+
 const deptnews = () => {
   
   return (
     <div className='w-full flex flex-col px-4 sm:px-20 py-[56px] justify-center'>
    <Subheading subtitle="News"/>
-   <div className='w-full flex flex-col items-center '>
-            <NewsCard
-              title="AOT"
-              date="2021-09-01"
-              description="The admit card has been released"
-            />
-           
-          </div>
-   </div>
+    </div>
   )
 }
 
