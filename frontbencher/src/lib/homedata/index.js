6f7bfccef447f9990f6b0d8dd9920a555e956145
bluecoder2003 <neelakshidas2003@gmail.com>
2024-05-17 01:56:07 +0530

    changes

--- frontbencher/src/lib/homedata/index.js
+++ frontbencher/src/lib/homedata/index.js
@@ -1,14 +1,29 @@
 export const sampleNews=
 [
     {
-        title: "Admission Procedure",
-        date: "Admission Procedure",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
+        title: "AOT-AGILE",
+        // date: "Admission Procedure",
+        description: "Greetings from AGILE. You are cordially invited to the Games Meet 2024. The Football Final match will start today at 4 PM. All the prizes and certificates for Football (Boys), Volleyball ( Boys & Girls) and Basketball ( Boys & Girls) will be given away today at the end of the Football Final Match. ",
     },
     {
-        title: "Admission Procedure",
-        date: "Admission Procedure",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
+        title: "OIC Examinations AOT",
+        // date: "Admission Procedure",
+        description: "This is to notify that students whose PPR forms have been verified can opt for the payment today. The portal is open for payment till 11:00 pm today (15-5-2024).",
+    },
+    {
+        title: "AOT Routine Committee ",
+        // date: "Admission Procedure",
+        description: "For B.Tech 8th Semester: Offline classes (according to the routine) will be held from 14.05.2024 to 18.05.2024.",
+    },
+    {
+        title: "OIC Examinations AOT ",
+        // date: "Admission Procedure",
+        description: "As notified by MAKAUT, PPR form fill-up for Odd Semester Examinations 2023-24 for B.Tech & MCA odd semester students will begin from May 11, 2024. The university notice is attached. The process will be as follows:1.         Students are advised to check for availability of ppr form and complete ppr form & fee submission process accordingly.2.         Students must finish submitting the ppr forms and requisite fees by May 14, 2024.",
+    },
+    {
+        title: "AOT Routine Committee ",
+        // date: "Admission Procedure",
+        description: "Dear Students,For B.Tech 2nd Semester, 4th Semester, 6th Semester and MCA 2nd Semester:  Find the attachments containing your routine for 11-5-2024. For B.Tech 8th Semester: Offline classes (according to the routine of Saturday) will be held on 11-05-2024.",
     },
 ];
 export const sampleEvents=[
@@ -27,8 +42,7 @@ export const sampleUpcomingEvents=[
 
 ];
 export const sampleAnnouncements=[
-    
-    {   title: "Admission Procedure",
-        
-        small_description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",}
+    // title: "Admission Procedure",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
+    // small_description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
 ];
