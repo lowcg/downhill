4da3050d709789e0dbe4ed68cbfca1f9f629c601
bluecoder2003 <neelakshidas2003@gmail.com>
2024-05-17 01:39:43 +0530

    changes

--- frontbencher/src/lib/homedata/index.js
+++ frontbencher/src/lib/homedata/index.js
@@ -5,6 +5,11 @@ export const sampleNews=
         date: "Admission Procedure",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
     },
+    {
+        title: "Admission Procedure",
+        date: "Admission Procedure",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
+    },
 ];
 export const sampleEvents=[
     {   title: "Admission Procedure",
