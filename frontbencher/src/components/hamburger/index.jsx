4a8df46c2853ea9386fb5f11c7a239f333d4c401
Sayan67 <sayan.das.21@aot.edu.in>
2024-05-19 01:39:49 +0530

    fixed hamburger

--- frontbencher/src/components/hamburger/index.jsx
+++ frontbencher/src/components/hamburger/index.jsx
@@ -5,8 +5,12 @@ const Hamburger = () => (
   <div className="relative px-4 h-full flex flex-col gap-4 overflow-scroll text-lg bg-deep-blue py-20">
     <div>
       <HamMenu label="Academics" active={true}>
-        <HamMenu label="UG Programs">
-          <HamMenuItem label="Admission Procedure" />
+        <Link href={"/ugprogramme"}>
+          <HamMenu label="UG Programs"></HamMenu>
+        </Link>
+        <Link href={"/pgprogramme"}>
+          <HamMenu label="PG Programs">
+            {/* <HamMenuItem label="Admission Procedure" />
           <HamMenuItem label="B. Tech Fees Structure" />
           <HamMenuItem label="B. Tech (Lateral) Fees Structure" />
           <HamMenuItem label="Rules for Fees Payment" />
@@ -17,22 +21,9 @@ const Hamburger = () => (
           <HamMenuItem label="Activities Beyond Academics" />
           <HamMenuItem label="Academic Calendar" />
           <HamMenuItem label="Regulations" />
-          <HamMenuItem label="Curriculum" />
-        </HamMenu>
-        <HamMenu label="PG Programs">
-          <HamMenuItem label="Admission Procedure" />
-          <HamMenuItem label="B. Tech Fees Structure" />
-          <HamMenuItem label="B. Tech (Lateral) Fees Structure" />
-          <HamMenuItem label="Rules for Fees Payment" />
-          <HamMenuItem label="Documents for Admission" />
-          <HamMenuItem label="Financial Aid & Support" />
-          <HamMenuItem label="Comprehensive Academic System" />
-          <HamMenuItem label="Academic Performance" />
-          <HamMenuItem label="Activities Beyond Academics" />
-          <HamMenuItem label="Academic Calendar" />
-          <HamMenuItem label="Regulations" />
-          <HamMenuItem label="Curriculum" />
-        </HamMenu>
+        <HamMenuItem label="Curriculum" /> */}
+          </HamMenu>
+        </Link>
         <HamMenu label="Resources">
           <Link href="https://aot.edu.in/library/">
             <HamMenuItem label="Library" />
@@ -42,28 +33,38 @@ const Hamburger = () => (
           </Link>
         </HamMenu>
       </HamMenu>
+
       <Link href="/departments">
         <HamMenu label="Departments" />
       </Link>
+
       <Link href="/placement">
         <HamMenu label="Placements" />
       </Link>
-      <HamMenu label="Research" active={true}>
-        <HamMenuItem label="Current Research" />
-        <HamMenuItem label="Past Research" />
-        <HamMenuItem label="Research Facilities" />
-      </HamMenu>
+
+      <Link href="/research">
+        <HamMenu label="Research" active={true}></HamMenu>
+      </Link>
+
       <HamMenu label="Students" active={true}>
-        <HamMenuItem label="Life@AOT" />
+        <Link href={"/students/lifeataot"}>
+          <HamMenuItem label="Life@AOT" />
+        </Link>
+        <Link href={'/students/campusfacilities'}>
         <HamMenuItem label="Campus Facilities" />
+        </Link>
+
         <HamMenuItem label="Career Services" />
-        <HamMenuItem label="Events" />
       </HamMenu>
 
       <HamMenu label="Faculty" active={true}>
-        <Link href="/faculty/list"><HamMenuItem label="Faculty List" /></Link>
+        <Link href="/faculty/list">
+          <HamMenuItem label="Faculty List" />
+        </Link>
         <HamMenuItem label="Faculty Positions" />
-        <Link href="/faculty/career"><HamMenuItem label="Career Benefits" /></Link>
+        <Link href="/faculty/career">
+          <HamMenuItem label="Career Benefits" />
+        </Link>
       </HamMenu>
     </div>
   </div>
