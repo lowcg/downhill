5c0f9f6abbd446bc4784136241b1ba8832773f83
Indrani Som <indranisom39@gmail.com>
2024-06-05 13:42:20 +0530

    added the lobrary page and the chatbot

---
+++ frontbencher/src/lib/librarydata/index.js
@@ -0,0 +1,73 @@
+export const departments = [
+    {
+      CSE: [
+        {
+          Sem1: [
+            {
+              image: "/assets/heroBg.png",
+              pdf: "/path/to/book1.pdf",
+              name: "Pal das",
+            },
+            {
+              image: "/assets/heroBg.png",
+              pdf: "/path/to/book1.pdf",
+              name: "pal das2.0",
+            },
+          ],
+          Sem2: [
+            {
+              image: "/assets/EEE-2.webp",
+              pdf: "/path/to/book1.pdf",
+              name: "Pal das",
+            },
+            {
+              image: "/assets/heroBg.png",
+              pdf: "/path/to/book1.pdf",
+              name: "alu das",
+            },
+          ],
+        },
+      ],
+      ECE: [
+        {
+          Sem1: [
+            {
+              image: "/assets/heroBg.png",
+              pdf: "/path/to/book1.pdf",
+              name: "Book 1",
+            },
+            {
+              image: "/path/to/image1.jpg",
+              pdf: "/path/to/book1.pdf",
+              name: "Book 1",
+            },
+          ],
+          Sem2: [
+            {
+              image: "/path/to/image1.jpg",
+              pdf: "/path/to/book1.pdf",
+              name: "Book 1",
+            },
+            {
+              image: "/path/to/image1.jpg",
+              pdf: "/path/to/book1.pdf",
+              name: "Book 1",
+            },
+          ],
+          Sem3: [
+            {
+              image: "/path/to/image1.jpg",
+              pdf: "/path/to/book1.pdf",
+              name: "Book 1",
+            },
+            {
+              image: "/path/to/image1.jpg",
+              pdf: "/path/to/book1.pdf",
+              name: "Book 1",
+            },
+          ],
+        },
+      ],
+      EEE: [{}],
+    },
+  ];
